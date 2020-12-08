367041f0f9773cef0cd412d11ce559b3967bcd79
Aidin Rasti <aidin.rasti@gmail.com>
2020-12-09 00:46:47 +0330

    more regexps

--- clean.js
+++ clean.js
@@ -22,14 +22,15 @@ async function concatRawDataset(pg) {
 
       try {
         const regs = [
-          [/^\n/g, ""], // remove empty lines
+          [/^\s*\n/g, ""], // remove empty lines
           [/^.*(├|└|─).*\n/g, ""], // remove dependency trees
           [
             /--- End of stack trace from previous location where exception was thrown ---/gi,
             "",
           ], //remove
           [/--- End of inner exception stack trace ---/gi, ""], //remove
-          [/^\n/g, ""], // remove empty lines again
+          [/^`(.*\n){0,}?.*`/g, ""], // remove
+          [/^\s*\n/g, ""], // remove empty lines again
 
           [
             /(?:[\uD83C\uDF00-\uD83D\uDDFF]|[\uD83E\uDD00-\uD83E\uDDFF]|[\uD83D\uDE00-\uD83D\uDE4F]|[\uD83D\uDE80-\uD83D\uDEFF]|[\u2600-\u26FF]\uFE0F?|[\u2700-\u27BF]\uFE0F?|\u24C2\uFE0F?|[\uD83C\uDDE6-\uD83C\uDDFF]{1,2}|[\uD83C\uDD70\uD83C\uDD71\uD83C\uDD7E\uD83C\uDD7F\uD83C\uDD8E\uD83C\uDD91-\uD83C\uDD9A]\uFE0F?|[\u0023\u002A\u0030-\u0039]\uFE0F?\u20E3|[\u2194-\u2199\u21A9-\u21AA]\uFE0F?|[\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55]\uFE0F?|[\u2934\u2935]\uFE0F?|[\u3030\u303D]\uFE0F?|[\u3297\u3299]\uFE0F?|[\uD83C\uDE01\uD83C\uDE02\uD83C\uDE1A\uD83C\uDE2F\uD83C\uDE32-\uD83C\uDE3A\uD83C\uDE50\uD83C\uDE51]\uFE0F?|[\u203C\u2049]\uFE0F?|[\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE]\uFE0F?|[\u00A9\u00AE]\uFE0F?|[\u2122\u2139]\uFE0F?|\uD83C\uDC04\uFE0F?|\uD83C\uDCCF\uFE0F?|[\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA]\uFE0F?)/,
@@ -44,7 +45,7 @@ async function concatRawDataset(pg) {
             "  ",
           ], // replace emoticons with double space
 
-          [/^\n/g, ""], // remove empty lines
+          [/^\s*\n/g, ""], // remove empty lines
 
           // logs
           [/^.*Error:.+\n(npm ERR!.+\n){1,}/g, ""], // remove, node js error log
@@ -83,7 +84,7 @@ async function concatRawDataset(pg) {
           [/^[\d]{1,} rows in set.+\n/g, ""], // remove
           [/\(\d+ rows\)/g, ""], // remove
 
-          [/^\n/g, ""], // remove empty lines
+          [/^\s*\n/g, ""], // remove empty lines
 
           // tables
           [/(\+|\|)---.+---(\+|\|)/g, ""], // remove
@@ -91,41 +92,48 @@ async function concatRawDataset(pg) {
           [/\|.*\|/g, ""], // remove
           [/---.+---/g, ""], // remove
 
-          [/^\n/g, ""], // remove empty lines
+          [/^\s*\n/g, ""], // remove empty lines
 
           // comments
-          // /\/\/ .*/, // remove, after file path
           [/\/\* .+ \*\//g, " "], // remove
-          [/\/\*.+\*\//, " "], // remove
-          /\/\*.+\//, // remove
-          /^##.*/, // remove XXXXXXXX
-          /\*\\.*\n/, // remove XXXXXXX
-          /\/\*.*?\*\//, // remove check**
-          /\/\*(.*\n){0,200}?.*\*\//, // remove XXXXXXXX
+          [/\/\*.+\*\//g, " "], // remove
+          [/^##+[^#]+##+/g, " "],
+          [/^\*\*+[^\*]+\*\*+/g, " "],
+
+          [/^\s*\n/g, ""], // remove empty lines
 
           // gibber
-          /^(.+=()?.+\n){3,}/, // remove
-          /^(.{0,40}:()?.{0,50}\n){3,}/, // remove
+          [/^(.+=()?.+\n){3,}/g, ""], // remove
+          [/^\s*\n/g, ""], // remove empty lines
+          [/^(.{0,40}:()?.{0,50}\n){3,}/g, ""], // remove env like configs (os: ubuntu)
+          [/^\s*\n/g, ""], // remove empty lines
 
-          /```.*?```/, // remove
-          /`.*?`/, // remove
-          /```(.*\n){0,200}?.*```/, // remove
-          /`(.*\n){0,200}?.*`/, // remove
-          /\{.*?\}/, // remove
-          /\{(.*\n){0,200}?.*\}/, // remove
-          /\[.*?\]/, // remove
-          /\[(.*\n){0,200}?.*\]/, // remove
-          /".*?"/, //remove
-          /<.*?>/, //remove XXXXXXXXXXX is wrong
-          /<.+>(.*\n){0,200}?.*<\/.+>/, // remove XXXXXXXXXXXXX
+          [/```.*?```/g, " "], // remove ```
+          [/^\s*\n/g, ""], // remove empty lines
+          [/`.*?`/g, " "], // remove `...`
+          [/^\s*\n/g, ""], // remove empty lines
 
-          // tags
-          /<\?php.+\?>/, // remove XXXXXXXXXXXX
-          /<\?.+\?>>/, // remove
-          /<\?= .+ ?>/, // tags
+          [/<(.+)( .+?)?>(.*\n){0,200}?.*<\/\1>/g, ""], // remove proper html tags, test in code XXXXXXXX
+          [/^\s*\n/g, ""], // remove empty lines
 
-          ///(^.+\.go.*\n){3,}/, //apply after uri reg exp
-          /<\?php .+ ?>/, // XXXXXXXXXXXX
+          [/<\?php(.*\n){0,200}?.*\?>/g, ""], // remove multiline php tags
+          [/<\?php.+\?>/g, " "], // remove php tags
+          [/<\?(.*\n){0,200}?.*\?>/g, " "], // remove tags
+          [/<\?.+\?>/g, ""], // remove
+
+          [/^\s*\n/g, ""], // remove empty lines
+
+          [/\[(.*\n){0,200}?.*\]/g, " "], // remove
+          [/^\s*\n/g, ""], // remove empty lines
+          [/^.+\{(.*\n){0,200}?.*\}/g, ""], // remove
+          [/^\s*\n/g, ""], // remove empty lines
+          [/\[(.*\n){0,200}?.*\]/g, " "], // remove
+          [/^\s*\n/g, ""], // remove empty lines
+          [/^.+\{(.*\n){0,200}?.*\}/g, ""], // remove
+          [/^\s*\n/g, ""], // remove empty lines
+
+          [/\{.*?\}/g, " "], // remove {}
+          [/\[.*?\]/g, " "], // remove []
 
           // exceptions
           /Caused by:.+\n(^\s+at .*\n){0,}\s+\.\.\..+/,
